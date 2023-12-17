ca786dc278b1a5b1f6d0340cfb6908b7869b7c77
vladyslav.stashkevyc <vladyslav.stashkevych@everymatrix.com>
2023-12-17 22:18:37 +0200

    Add project files.

---
+++ ThreeDSecure/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using Stripe;
+using Stripe.Checkout;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+
+        StripeConfiguration.ApiKey = "sk_test_26PHem9AhJZvU623DfE1x4sd";
+
+        try
+        {
+            var sessionOptions = new SessionCreateOptions
+            {
+                PaymentMethodTypes = new List<string>
+                {
+                    "card",
+                },
+                LineItems = new List<SessionLineItemOptions>
+                {
+                    new SessionLineItemOptions
+                    {
+                        PriceData = new SessionLineItemPriceDataOptions
+                        {
+                            Currency = "usd",
+                            ProductData = new SessionLineItemPriceDataProductDataOptions
+                            {
+                                Name = "Товар",
+                            },
+                            UnitAmount = 1000,
+                        },
+                        Quantity = 1,
+                    },
+                },
+                Mode = "payment",
+                SuccessUrl = "https://example.com/success",
+                CancelUrl = "https://example.com/cancel",
+            };
+
+            var sessionService = new SessionService();
+            var session = sessionService.CreateAsync(sessionOptions).Result;
+
+            Console.WriteLine(session.Id);
+
+            Console.WriteLine($"URL для оплати: {session.Url}");
+        }
+        catch (StripeException e)
+        {
+            Console.WriteLine($"Помилка Stripe: {e.Message}");
+        }
+    }
+}
